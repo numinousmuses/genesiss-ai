50c5524fb998d59cad050777993ec0e75ffc03b5
twizzworld <132231765+twizzworld@users.noreply.github.com>
2024-09-22 02:01:11 -0400

    finished imagegen endpoint

---
+++ src/app/api/imagegen/route.ts
@@ -0,0 +1,75 @@
+/* eslint-disable */
+import { NextRequest, NextResponse } from 'next/server';
+import Cors from 'cors';
+import { 
+  verifyApiKey, 
+  imageGenerationAgent
+} from '@/lib/utils';
+
+// Initialize the CORS middleware
+const cors = Cors({
+    methods: ['POST'],
+    origin: '*', // Allow all origins (adjust as necessary for your security needs)
+    allowedHeaders: ['x-api-key', 'Content-Type'],
+  });
+
+  // Helper method to run middleware
+async function runMiddleware(req: NextRequest, res: NextResponse, fn: Function) {
+    return new Promise((resolve, reject) => {
+      fn(req, res, (result: any) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+        return resolve(result);
+      });
+    });
+  }
+
+export const POST = async (req: NextRequest, res: NextResponse) => {
+
+    // Run CORS middleware
+    await runMiddleware(req, res, cors);
+
+    if (req.method === 'POST') {
+        
+        try {
+            
+            const { ak, prompt, height, width } = await req.json();
+    
+            if (!ak || !prompt) {
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({ error: 'Missing required parameters' }),
+                };
+            }
+    
+            if (!await verifyApiKey(ak)) {
+                return {
+                    statusCode: 401,
+                    body: JSON.stringify({ error: 'Unauthorized' }),
+                };
+            }
+    
+            const res = await imageGenerationAgent(prompt, height, width)
+    
+            return NextResponse.json({
+                statusCode: 200,
+                body: JSON.stringify({ imageeURL: res }),
+            })
+    
+        } catch (error) {
+            console.error("ImageGen Endpoint error: " + JSON.stringify(error, null, 2))
+            return NextResponse.json({
+                statusCode: 400,
+                body: JSON.stringify({ error: 'Internal Server Error' }),
+            })
+        }
+
+    } else {
+
+        return NextResponse.json({
+            statusCode: 405,
+            body: JSON.stringify({ error: 'Method Not Allowed' }),
+        });
+    }
+}
